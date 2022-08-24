a5c8790a273c94e9f3b06c32f56e7f7fe3dc6869
GamezLe10 <wlgs_1992@hotmail.com>
2022-08-24 18:46:51 -0500

    Primer punto - Paso 1,2,3
    http://javaonfly.blogspot.com/2018/09/layered-architecture-up-and-running.html
    
    Layered Architecture Up and Running just in 5 minutes:: Spring Boot Part 1
    
    Se crea la base del proyecto

---
+++ src/main/java/com/example/LaboratorioI/LaboratorioIApplication.java
@@ -0,0 +1,13 @@
+package com.example.LaboratorioI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratorioIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratorioIApplication.class, args);
+	}
+
+}
