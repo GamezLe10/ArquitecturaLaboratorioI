6657fe4f921c6909f4abb4c2545dfce24db2db80
GamezLe10 <wlgs_1992@hotmail.com>
2022-08-24 19:49:05 -0500

    Punto final
    Consumo del servicio para la creación de empleados y consumo del servicio

---
+++ src/main/java/com/example/LaboratorioI/service/EmployeeService.java
@@ -0,0 +1,36 @@
+package com.example.LaboratorioI.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.LaboratorioI.entity.Employee;
+import com.example.LaboratorioI.repository.EmployeeRepositiry;
+
+@Service
+public class EmployeeService {
+	@Autowired
+	EmployeeRepositiry repo;
+
+	public Employee addEmployee(Employee emp) {
+		emp = repo.save(emp);
+		System.out.println("Employee saved::" + emp);
+		return emp;
+	}
+
+	public Optional<Employee> findEmployeeById(Long empId) {
+		Optional<Employee> emp = repo.findById(empId);
+		System.out.println("Employee found::" + emp);
+		return emp;
+	}
+
+	public Iterable<Employee> findAllEmployee() {
+		return repo.findAll();
+	}
+
+	public void deleteEmployeeById(Employee emp) {
+		repo.delete(emp);
+		System.out.println("Employee deleted::" + emp);
+	}
+}
