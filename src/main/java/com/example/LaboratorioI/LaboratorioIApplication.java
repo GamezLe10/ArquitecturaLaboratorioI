92a356012af1b1098845877857aece1e241815c6
GamezLe10 <wlgs_1992@hotmail.com>
2022-08-24 19:29:43 -0500

    Paso 8 y paso 9
    Paso 8 se crea la funcionalidad para cargar los empleados en la clase InitDataLoader
    Paso 9 Se crea un método en la clase main de la aplicación para cargar los empleados en la base de datos H2 LaboratorioIApplication

--- src/main/java/com/example/LaboratorioI/LaboratorioIApplication.java
+++ src/main/java/com/example/LaboratorioI/LaboratorioIApplication.java
@@ -1,13 +1,24 @@
 package com.example.LaboratorioI;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.example.LaboratorioI")
 public class LaboratorioIApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LaboratorioIApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner loadData(InitDataLoader loader) {
+		return args -> loader.loadData();
 	}
 
 }
